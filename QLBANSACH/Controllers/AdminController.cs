54e7a7da6070fefa6c789e784cb275a5cc8c2c28
linhhl2807 <118676532+linhhl2807@users.noreply.github.com>
2023-05-25 23:14:50 +0700

    TH_Manguonmo

---
+++ QLBANSACH/Controllers/AdminController.cs
@@ -0,0 +1,198 @@
+﻿using QLBANSACH.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+using System.IO;
+using System.Web.UI.WebControls;
+
+namespace QLBANSACH.Controllers
+{
+    public class AdminController : Controller
+    {
+        dbQLBanSachDataContext db = new dbQLBanSachDataContext();
+
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Sach(int? page)
+        {
+            int pageNumber = (page ?? 1);
+            int pageSize = 7;
+            return View(db.SACHes.ToList().OrderBy(n => n.Masach).ToPagedList(pageNumber, pageSize));
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            var tendn = collection["username"];
+            var matkhau = collection["password"];
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                Admin ad = db.Admins.SingleOrDefault(n => n.UserAdmin == tendn && n.PassAdmin == matkhau);
+                if (ad != null)
+                {
+                    Session["Taikhoanadmin"] = ad;
+                    return RedirectToAction("Index", "Admin");
+                }
+                else
+                    ViewBag.Thongbao = "Tên đăng nhập hoặc mật khẩu không đúng";
+
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Themmoisach()
+        {
+            ViewBag.MaCD = new SelectList(db.CHUDEs.ToList().OrderBy(n => n.TenChuDe), "MaCD", "TenChude" );
+            ViewBag.MaNXB = new SelectList(db.NHAXUATBANs.ToList().OrderBy(n => n.TenNXB), "MaNXB", "TenNXB");
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Themmoisach(SACH sach, HttpPostedFileBase fileupload)
+        {
+            ViewBag.MaCD = new SelectList(db.CHUDEs.ToList().OrderBy(n => n.TenChuDe), "MaCD", "TenChude");
+            ViewBag.MaNXB = new SelectList(db.NHAXUATBANs.ToList().OrderBy(n => n.TenNXB), "MaNXB", "TenNXB");
+            if (fileupload == null)
+            {
+                ViewBag.Thongbao = "Vui lòng chọn ảnh bìa";
+                return View();
+            }
+            else
+            {
+                if (ModelState.IsValid)
+                {
+                    var filename = Path.GetFileName(fileupload.FileName);
+                    var path = Path.Combine(Server.MapPath("~/images"), filename);
+                    if (System.IO.File.Exists(path))
+                    {
+                        ViewBag.Thongbao = "Hình ảnh đã tồn tại";
+                    }
+                    else
+                    {
+                        fileupload.SaveAs(path);
+                    }
+                    sach.Anhbia = filename;
+                    db.SACHes.InsertOnSubmit(sach);
+                    db.SubmitChanges();
+                }
+                return RedirectToAction("Sach");
+            }
+
+
+        }
+
+        public ActionResult Chitietsach(int id)
+        {
+            SACH sach = db.SACHes.SingleOrDefault(n => n.Masach == id);
+            ViewBag.Masach = sach.Masach;
+            if (sach == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(sach);
+        }
+
+        [HttpGet]
+        public ActionResult Xoasach(int id)
+        {
+            SACH sach = db.SACHes.SingleOrDefault(n => n.Masach == id);
+            ViewBag.Masach = sach.Masach;
+            if (sach == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+          
+            return View(sach);
+        }
+
+        [HttpPost, ActionName("Xoasach")]
+        public ActionResult xacnhanxoa(int id)
+        {
+            //Lay ra doi tuong sach can xoa theo ma
+            SACH sach = db.SACHes.SingleOrDefault(n => n.Masach == id);
+            ViewBag.Masach = sach.Masach;
+            if (sach == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.SACHes.DeleteOnSubmit(sach);
+            db.SubmitChanges();
+            return RedirectToAction("Sach");
+        }
+        [HttpGet]
+
+        public ActionResult suasach(int id)
+        {
+            //Lay ra doi tuong sach theo mã
+            SACH sach = db.SACHes.SingleOrDefault(n => n.Masach == id);
+            if (sach == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            //Đưa dữ liệu vào dropdown
+            ViewBag.MaCD = new SelectList(db.CHUDEs.ToList().OrderBy(n => n.TenChuDe), "MaCD", "TenChude", sach.MaCD);
+            ViewBag.MaNXB = new SelectList(db.NHAXUATBANs.ToList().OrderBy(n => n.TenNXB), "MaNXB", "TenNXB", sach.MaNXB);
+            return View(sach);
+
+        }
+
+        //Chỉnh sửa sản phẩm
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult suasach(int id, SACH sach, HttpPostedFileBase fileUpload)
+        {
+            SACH sach1 = db.SACHes.SingleOrDefault(n => n.Masach == id);
+            //Đưa dữ liệu vào dropdown
+            ViewBag.MaCD = new SelectList(db.CHUDEs.ToList().OrderBy(n => n.TenChuDe), "MaCD", "TenChude");
+            ViewBag.MaNXB = new SelectList(db.NHAXUATBANs.ToList().OrderBy(n => n.TenNXB), "MaNXB", "TenNXB");
+            //Kiem tra duong dan file
+            //Luu ten fie
+            if (fileUpload != null)
+            {
+                if (ModelState.IsValid)
+                {
+                    var fileName = Path.GetFileName(fileUpload.FileName);
+                    //Luu duong dan cua file
+                    var path = Path.Combine(Server.MapPath("~/images"), fileName);
+                    fileUpload.SaveAs(path);
+                    sach1.Anhbia = fileName;
+                }
+            }
+            else
+            {
+                ViewBag.Thongbao = "Vui lòng chọn ảnh bia";
+                return View(sach);
+            }
+            //Luu vao CSDL
+            UpdateModel(sach1);
+            UpdateModel(sach);
+            db.SubmitChanges();
+            return RedirectToAction("Sach");
+        }
+    }
+    }
+
---
+++ QLBANSACH/Controllers/BookStoreController.cs
@@ -0,0 +1,59 @@
+﻿using QLBANSACH.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;   
+using PagedList;
+using PagedList.Mvc;
+
+namespace QLBANSACH.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        dbQLBanSachDataContext data = new dbQLBanSachDataContext();
+
+        public dbQLBanSachDataContext Data { get => data; set => data = value; }
+
+        // GET: BookStore
+        private List<SACH> laysachmoi(int count)
+        {
+            return Data.SACHes.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
+        }
+
+        public ActionResult Index(int ? page)
+        {
+            int pageSize = 6;
+            int pageNum = (page ?? 1);
+            var sachmoi = laysachmoi(16);
+            return View(sachmoi.ToPagedList(pageNum,pageSize));
+        }
+        public ActionResult Chude()
+        {
+            var chude = from cd in data.CHUDEs select cd;
+            return PartialView(chude);
+        }
+        public ActionResult Nhaxuatban()
+        {
+            var nhaxuatban = from cd in data.NHAXUATBANs select cd;
+            return PartialView(nhaxuatban);
+        }
+        public ActionResult SPTheochude(int id)
+        {
+            var sach = from s  in data.SACHes where s.MaCD==id select s  ;
+            return View(sach);
+        }
+        public ActionResult SPTheoNXB(int id)
+        {
+            var sach = from s in data.SACHes where s.MaCD == id select s;
+            return View(sach);
+        }
+        public ActionResult Details(int id)
+        {
+            var sach = from s in data.SACHes 
+                       where s.Masach == id 
+                       select s;
+            return View(sach.Single());
+        }
+    }
+}
